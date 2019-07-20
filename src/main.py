261e82e715cac361ca4cd94bf491b21f2d199e59
Stefan <stefan.kandic@yahoo.com>
2019-07-20 02:59:31 +0200

    small changes to main module

--- src/main.py
+++ src/main.py
@@ -4,14 +4,7 @@ from src.Star.StarPSO import StarPSO
 from src.Ring.RingPSO import RingPSO
 from src.Regular.PSO import PSO
 
-
 def rosenbrock(X):
-    """
-    This R^2 -> R^1 function should be compatible with algopy.
-    http://en.wikipedia.org/wiki/Rosenbrock_function
-    A generalized implementation is available
-    as the scipy.optimize.rosen function
-    """
     x = X[0]
     y = X[1]
     a = 1. - x
