0a7b441d0e9b58a17b5b6f7a1ab8c5726a20ac2f
Stefan <stefan.kandic@yahoo.com>
2019-07-20 02:58:57 +0200

    small changes to main module

--- src/main.py
+++ src/main.py
@@ -5,63 +5,21 @@ from src.Ring.RingPSO import RingPSO
 from src.Regular.PSO import PSO
 
 
-def main():
-    while True:
-        print("Odabir pso algoritma:")
-        print("1. PSO")
-        print("2. PSO Ring")
-        print("3. PSO Star")
-        print("x. Izlaz")
-        inn = input(">>")
-        if inn == "x":
-            return
-        else:
-            t1 = time()
-            if inn == "1":
-                g, f = test_pso()
-            elif inn == "2":
-                g, f = test_ring()
-            elif inn == "3":
-                g, f = test_star()
-            else:
-                print("Neodgovarajuci znak!")
-                continue
-            dt = time() - t1
-            print("dt : ", dt, "fmin : ", f)
-            print("g = ", g)
-
-
-def input_parameters():
-    max_iter = eval(input("Unesite broj iteracija: "))
-    num_particles = eval(input("Unesite broj cestica: "))
-    i = input("Unesite 1 za konstantne koeficijente ili bilo sta drugo za promenljive koeficijente: ")
-    if i == "1":
-        const = True
-    else:
-        const = False
-    return max_iter, num_particles, const
-
-
-def test_pso():
-    max_iter, num_particles, const = input_parameters()
-    p = PSO(optimality_criterion, num_dimensions=60, num_particles=num_particles, max_iter=max_iter, display=True,
-            const=const)
-    return p.optimize()
-
-
-def test_ring():
-    max_iter, num_particles, const = input_parameters()
-    p = RingPSO(optimality_criterion, num_dimensions=60, num_particles=num_particles, max_iter=max_iter, display=True,
-                const=const)
-    return p.optimize()
-
-
-def test_star():
-    max_iter, num_particles, const = input_parameters()
-    p = StarPSO(optimality_criterion, num_dimensions=60, num_particles=num_particles, max_iter=max_iter, display=True,
-                const=const)
-    return p.optimize()
-
+def rosenbrock(X):
+    """
+    This R^2 -> R^1 function should be compatible with algopy.
+    http://en.wikipedia.org/wiki/Rosenbrock_function
+    A generalized implementation is available
+    as the scipy.optimize.rosen function
+    """
+    x = X[0]
+    y = X[1]
+    a = 1. - x
+    b = y - x*x
+    return a*a + b*b*100.
 
 if __name__ == '__main__':
-    main()
+    #main()
+    p = PSO(rosenbrock, num_dimensions=2, num_particles=100, max_iter=100)
+    position, value = p.optimize()
+    print('Optimum position: '+str(position) + '\nOptimum value: '+ str(value))
